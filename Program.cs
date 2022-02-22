e9b7421bd419dbb984e517908cb40676d855a050
MaksimBudeev <78585421+MaksimBudeev@users.noreply.github.com>
2022-02-22 18:14:09 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+
+namespace handh.school.test
+{
+    public class Program
+    {
+        public static int[][] TestTask(int n)
+        {
+            int[][] result;
+            try
+            {
+                result = new int[n][];
+                HashSet<int> sizeOfArrSet = new HashSet<int>();
+
+                int newSize;
+                Random r = new Random();
+
+                int i;
+
+                for (i = 0; i < n; i++)
+                {
+                    newSize = r.Next(1, n * 2);
+
+                    if (sizeOfArrSet.Contains(newSize))
+                    {
+                        i--;
+                        continue;
+                    }
+
+                    sizeOfArrSet.Add(newSize);
+
+                    result[i] = new int[newSize];
+                }
+
+                for (i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < result[i].Length; j++)
+                    {
+                        result[i][j] = r.Next(int.MinValue, int.MaxValue);
+                    }
+                }
+
+                Sort<int>.Compare min, max;
+
+                min = Sort<int>.Min;
+                max = Sort<int>.Max;
+
+                for (i = 0; i < result.Length; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Sort<int>.HeapSort(result[i], min);
+                    }
+                    else
+                    {
+                        Sort<int>.HeapSort(result[i], max);
+                    }
+                }
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"There was an exception: {ex.Message}");
+                return null;
+            }
+        }
+        public static void Main(string[] args)
+        {
+            int[][] arrOfArr;
+            int arrOfArrSize = Convert.ToInt32(args[0]);
+           
+            arrOfArr = TestTask(arrOfArrSize);
+
+            if(arrOfArr != null)
+            {
+                Console.WriteLine("Done.");
+            }
+            else
+            {
+                Console.WriteLine("arrOfArr is null.");
+            }
+        }
+    }
+}
---
+++ Sort.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace handh.school.test
+{
+    public class Sort<T> where T : IComparable<T>
+    {
+        public delegate bool Compare(T a, T b);
+
+        internal static bool Min(T a, T b)
+        {
+            if (a.CompareTo(b) <= 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        internal static bool Max(T a, T b)
+        {
+            if (b.CompareTo(a) < 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static void Heapify(T[] arr, int n, int i, Compare comp)
+        {
+            int largest = i;
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+
+            if (left < n && comp(arr[left], arr[largest]))
+            {
+                largest = left;
+            }
+ 
+            if (right < n && comp(arr[right], arr[largest]))
+            {
+                largest = right;
+            }
+             
+            if (largest != i)
+            {
+                T swap = arr[i];
+                arr[i] = arr[largest];
+                arr[largest] = swap;
+                Heapify(arr, n, largest, comp);
+            }
+        }
+
+        internal static void HeapSort(T[] arr, Sort<T>.Compare comp)
+        {
+            for (int i = arr.Length / 2 - 1; i >= 0; i--)
+            {
+                Heapify(arr, arr.Length, i, comp);
+            }
+
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                T temp = arr[0];
+                arr[0] = arr[i];
+                arr[i] = temp;
+                Heapify(arr, i, 0, comp);
+            }
+        }
+    }
+}
